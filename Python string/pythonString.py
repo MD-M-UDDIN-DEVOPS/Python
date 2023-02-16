d4b0146330602505cee811a0baef7fa87fea7ca3
minhaj <minhaj@gmail.com>
2023-02-15 21:48:59 -0500

    python_string

--- Python string/pythonString.py
+++ Python string/pythonString.py
@@ -5,15 +5,13 @@ world
 hello"""
 print(my_str_multi)
 
-my_new = "My name is Nishant Tiwari"
+my_new = "My name is Md_Uddin"
 
 print(my_new[0:10])
 print(my_new[4:10])
 print(my_new[-1])
 
 print(my_new[0:20:3])
-#reverse a str
+# reverse a str
 print(my_new[::-1])
 print(my_new[0:-4])
-
-
